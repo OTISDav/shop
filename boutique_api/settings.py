4f6582d13a350c9f8dd8e03d94e658338f576aaf
OTISDav <davidbotcholi2003@gmail.com>
2024-12-27 22:07:57 +0000

    ajout de la Db

--- boutique_api/settings.py
+++ boutique_api/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'boutique_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'saas',  # Nom de la base de données
+        'USER': 'postgres',  # Nom d'utilisateur
+        'PASSWORD': 'Otis2003',  # Mot de passe
+        'HOST': 'localhost',  # Host de la base de données
+        'PORT': '5432',  # Port
     }
 }
 
